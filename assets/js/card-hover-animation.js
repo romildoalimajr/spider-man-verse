3b3ca7caba7fe7fc1afcf3364c81cb7638fc0ad5
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-05 17:55:58 -0300

    Finalizados os efeitos nos cards.

--- assets/js/card-hover-animation.js
+++ assets/js/card-hover-animation.js
@@ -1,10 +1,12 @@
 
 function handleMouseEnter() {
     this.classList.add('s-card--hovered');
+    document.body.id = `${this.id}-hovered`;
 }
 
 function handleMouseLeave() {
     this.classList.remove('s-card--hovered');
+    document.body.id = '';
 }
 
 function addEventListenersToCards() {
